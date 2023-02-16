a90232338f247c33f978cd85f734c3e7ea5810a7
Adrienne Winter <adie.e.winter@gmail.com>
2023-02-16 17:10:52 +0200

    fixing stereo synch

--- Stereo_Synch_ROS.cpp
+++ Stereo_Synch_ROS.cpp
@@ -1,138 +1,103 @@
 // This script opens a rosbag file and saves synchronised camera topics to a struct
 // Adapted from http://wiki.ros.org/rosbag/Cookbook#Analyzing_Stereo_Camera_Data
 
+#include <boost/foreach.hpp>
+#include <deque>
+
 #include <ros/ros.h>
 #include <rosbag/bag.h>
 #include <rosbag/view.h>
-
-#include <boost/foreach.hpp>
-
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
 #include <message_filters/simple_filter.h>
-
 #include <sensor_msgs/Image.h>
-//#include <sensor_msgs/CameraInfo.h>
 
-//-------------------------CLASSES-------------------------------------------------------
-// A struct to hold the synchronized camera stereo data 
-class StereoData
-{
-public:
-  sensor_msgs::Image::ConstPtr image0, image1;
-  //sensor_msgs::CameraInfo::ConstPtr cam_info_0, cam_info_1;
-  
-  StereoData(const sensor_msgs::Image::ConstPtr &img0, 
-             const sensor_msgs::Image::ConstPtr &img1) 
-             //const sensor_msgs::CameraInfo::ConstPtr &info0, 
-             //const sensor_msgs::CameraInfo::ConstPtr &info1) :
-    image0(img0),
-    image1(img1),
-    //cam_info_0(info0),
-    //cam_info_1(info1)
-  {}
-};
+//-------------------------DEFINE IMAGE QUEUES-----------------------------------------------------
+// Create double ended queues to hold the synchronized images 
+std::deque<sensor_msgs::Image> imgQueue0;
+std::deque<sensor_msgs::Image> imgQueue1;
 
-
-// Inherits from message_filters::SimpleFilter<M> to use protected signalMessage function when reading from the rosbag
-template <class M>
-class BagSubscriber : public message_filters::SimpleFilter<M>
-{
-public:
-  void newMessage(const boost::shared_ptr<M const> &msg)
-  {
-    signalMessage(msg); // Call all registered callbacks, passing them the specified message
-  }
-};
 
 
 //-------------------------FUNCTIONS-------------------------------------------------------
 // Callback for synchronizing stereo messages and saving them in a struct
-void stereoSynchCallback(const sensor_msgs::Image::ConstPtr &img0, 
-              const sensor_msgs::Image::ConstPtr &img1) 
-              //const sensor_msgs::CameraInfo::ConstPtr &info0,
-              //const sensor_msgs::CameraInfo::ConstPtr &info1)
+void stereoSynchCallback(const sensor_msgs::Image::ConstPtr& img0, const sensor_msgs::Image::ConstPtr& img1) 
 {
-  StereoData sd(img0, img1); //info0, info1); // struct - should this not be stereo_dataset_?
+  imgQueue0.push_back(img0);
+  imgQueue1.push_back(img1);
 
-  // stereo_dataset_ is class variable to store data
-  stereo_dataset_.push_back(sd);
+  // maybe just write to the new bag here?
+
 }
  
+ 
 
-// Load rosbag and Synchronize the image topics before saving the messages into a struct
-void loadBag(const std::string &filename)
+// Load rosbag, iterate through the messages on each topic and call the synchronizer callback
+void readBag(const std::string &filename)
 {
   rosbag::Bag bag;
   bag.open(filename, rosbag::bagmode::Read);
   
-  std::string cam0_image = "video_source_0/raw";
-  std::string cam1_image = "video_source_1/raw";
-  //std::string cam0_info = cam0 + "/camera_info";
-  //std::string cam1_info = cam1 + "/camera_info";
+  // define the camera ROS topics
+  std::string cam0_topic = "video_source_0/raw";
+  std::string cam1_topic = "video_source_1/raw";
   
-  // Image topics to load
+  // create a vector of topics to iterate through
   std::vector<std::string> topics;
-  topics.push_back(cam0_image);
-  topics.push_back(cam1_image);
-  //topics.push_back(cam0_info);
-  //topics.push_back(cam1_info);
+  topics.push_back(cam0_topic);
+  topics.push_back(cam1_topic);
   
   rosbag::View view(bag, rosbag::TopicQuery(topics));
   
-  // Set up fake subscribers to capture images
-  BagSubscriber<sensor_msgs::Image> img0_sub, img1_sub;
-  //BagSubscriber<sensor_msgs::CameraInfo> info0_sub, info1_sub;
-  
-  // Use time synchronizer to make sure we get properly synchronized images
-  message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::Image> sync(img0_sub, img1_sub, 25);
-  sync.registerCallback(boost::bind(&stereoSynchCallback, _1, _2));
-  
-  // Load all messages into our stereo dataset
+  // Load all messages into our image datasets
   BOOST_FOREACH(rosbag::MessageInstance const msg, view)
   {
-    if (msg.getTopic() == cam0_image || ("/" + msg.getTopic() == cam0_image))
+    if (msg.getTopic() == cam0_topic || ("/" + msg.getTopic() == cam0_topic))
     {
       sensor_msgs::Image::ConstPtr img0 = msg.instantiate<sensor_msgs::Image>();
       if (img0 != NULL)
-        img0_sub.newMessage(img0); // call the stereoSynchCallback
+        img0_sub.signalMessage(img0); // call the stereoSynchCallback
     }
     
-    if (msg.getTopic() == cam1_image || ("/" + msg.getTopic() == cam1_image))
+    if (msg.getTopic() == cam1_topic || ("/" + msg.getTopic() == cam1_topic))
     {
       sensor_msgs::Image::ConstPtr img1 = msg.instantiate<sensor_msgs::Image>();
       if (img1 != NULL)
-        img1_sub.newMessage(img1);
+        img1_sub.signalMessage(img1);
     }
-    
-    //if (m.getTopic() == cam0_info || ("/" + m.getTopic() == cam0_info))
-    //{
-      //sensor_msgs::CameraInfo::ConstPtr info0 = m.instantiate<sensor_msgs::CameraInfo>();
-      //if (info0 != NULL)
-        //info0_sub.newMessage(info0);
-    //}
-    
-    //if (m.getTopic() == cam1_info || ("/" + m.getTopic() == cam1_info))
-    //{
-      //sensor_msgs::CameraInfo::ConstPtr info1 = m.instantiate<sensor_msgs::CameraInfo>();
-      //if (info1 != NULL)
-        //info1_sub.newMessage(info1);
-    //}
   }
   bag.close();
 }
 
 
 
+void writeSynchedBag()
+{
+  // Use the image queues to write to a new rosbag if possibile
+}
+
+
+
 //-------------------------MAIN-------------------------------------------------------
+//TODO add argc and argv implementation for rosbag path/name
 int main(int argc, char** argv)
 {
    // Create a ROS node
    ros::init(argc, argv, "synchronizer_node");
    ros::NodeHandle nh;
+
+   // Set up message_filters subscribers to capture images
+   message_filters::Subscriber<sensor_msgs::Image> img0_sub(nh, ); 
+   img1_sub;
+  
+   // Use time synchronizer to make sure we get properly synchronized images
+   message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::Image> sync(img0_sub, img1_sub, 25);
+   sync.registerCallback(boost::bind(&stereoSynchCallback, _1, _2));
  
-   loadBag("~/Documents/ROS_Workspace/rosbags")
+   readBag("~/Documents/ROS_Workspace/rosbags/AllSensors_600x600_15fps_100Hz_1Hz_2023-02-15.bag");
+   //writeSynchedBag();
  
    ros::spin(); // is this needed?
    return 0;
 }
+
