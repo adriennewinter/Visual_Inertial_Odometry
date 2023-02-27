ed133638162390578edf27ca6dcb6ac97861ea96
Adrienne Winter <adie.e.winter@gmail.com>
2023-02-27 13:21:08 +0200

    using one topic for IMU

--- Stereo_Inertial_Synch.cpp
+++ Stereo_Inertial_Synch.cpp
@@ -30,34 +30,33 @@
 #include <message_filters/sync_policies/approximate_time.h>
 #include <message_filters/simple_filter.h>
 #include <sensor_msgs/Image.h> // camera image messages
-#include <geometry_msgs/Vector3Stamped.h> // accelerometer and gyro messages
+#include <sensor_msgs/Imu.h> // IMU messages
  
 using namespace std;
 
 //-------------------------GLOBAL VARIABLES-----------------------------------------------------
 std::string rosbagFolderPath = "/home/user/Documents/ROS_Workspace/rosbags";
-std::string unsynchedBagName = "AllSensors_600x600_15fps_100Hz_1Hz_2023-02-15.bag";
+std::string unsynchedBagName = "AllSensors_600x600_15fps_100Hz_1Hz_23-02-2023.bag";
 std::string cam0_topic = "/video_source_0/raw";
 std::string cam1_topic = "/video_source_1/raw";
-std::string accel_topic = "/imu/acceleration";
-std::string gyro_topic = "/imu/angular_velocity";
+std::string imu_topic = "/imu/data";
 
 struct stereo_inertial {
   sensor_msgs::Image img0;
   sensor_msgs::Image img1;
-  geometry_msgs::Vector3Stamped accel;
-  geometry_msgs::Vector3Stamped gyro;
+  sensor_msgs::Imu imu;
 };
 
-int synch_cnt, i, j, k, l = 0; 
-std::vector<stereo_inertial> SynchedMsgsVec;
+int synch_cnt, i, j, k = 0; 
+std::deque<stereo_inertial> SynchedMsgs;
+std::deque<sensor_msgs::Imu> imuBuffer;
 
 
 
 
 
 //-------------------------CALLBACKS-------------------------------------------------------
-void SynchCallback(const sensor_msgs::Image::ConstPtr& img0_msg, const sensor_msgs::Image::ConstPtr& img1_msg, const geometry_msgs::Vector3Stamped::ConstPtr& accel_msg, const  geometry_msgs::Vector3Stamped::ConstPtr& gyro_msg)
+void SynchCallback(const sensor_msgs::Image::ConstPtr& img0_msg, const sensor_msgs::Image::ConstPtr& img1_msg, const sensor_msgs::Imu::ConstPtr& imu_msg)
 // Callback for synchronizing stereo messages with imu messages - higher IMU rate gets lost 
 { 
   struct stereo_inertial StereoInertialStruct;
@@ -65,13 +64,19 @@ void SynchCallback(const sensor_msgs::Image::ConstPtr& img0_msg, const sensor_ms
   // Insert synched messages into the struct
   StereoInertialStruct.img0 = *img0_msg;
   StereoInertialStruct.img1 = *img1_msg;
-  StereoInertialStruct.accel = *accel_msg;
-  StereoInertialStruct.gyro = *gyro_msg;
+  StereoInertialStruct.imu = *imu_msg;
 
-  // Insert the struct into the vector
-  SynchedMsgsVec.push_back(StereoInertialStruct);
+  // Insert the struct into the deque
+  SynchedMsgs.push_back(StereoInertialStruct);
 
   synch_cnt += 1;
+}
+
+
+
+void IMU_Buffer_Callback(const sensor_msgs::Imu::ConstPtr& imu_msg)
+{
+  imuBuffer.push_back(*imu_msg);
 }
 
 
@@ -81,30 +86,25 @@ void writeToBag(rosbag::Bag& synched_bag)
 // Write synchronized messages to the synched rosbag
 {
   sensor_msgs::Image img0_msg, img1_msg;
-  geometry_msgs::Vector3Stamped accel_msg, gyro_msg;
+  sensor_msgs::Imu imu_msg;
   struct stereo_inertial StereoInertialStruct;
 
-  if(!SynchedMsgsVec.empty())
+  if(!SynchedMsgs.empty())
   {
-    // Get latest synched messages from the vector and then delete them from the vector 
-    StereoInertialStruct = SynchedMsgsVec.front();
-    SynchedMsgsVec.erase(SynchedMsgsVec.begin());
+    // Get latest synched messages from the deque and delete them from the deque 
+    StereoInertialStruct = SynchedMsgs.front();
+    SynchedMsgs.pop_front();
 
     img0_msg = StereoInertialStruct.img0;
     img1_msg = StereoInertialStruct.img1;
-    accel_msg = StereoInertialStruct.accel;
-    gyro_msg = StereoInertialStruct.gyro;
+    imu_msg = StereoInertialStruct.imu;
 
-    cout << "img0 timestamp = " << img0_msg.header.stamp << endl;
-    cout << "img1 timestamp = " << img1_msg.header.stamp << endl;
-    cout << "accel timestamp = " << accel_msg.header.stamp << endl;
-    cout << "gyro timestamp = " << gyro_msg.header.stamp << endl;
+    search_imuBuffer(imu_msg);
 
     // Write a synched set of messages to a rosbag
     synched_bag.write(cam0_topic, img0_msg.header.stamp, img0_msg); 
     synched_bag.write(cam1_topic, img1_msg.header.stamp, img1_msg);
-    synched_bag.write(accel_topic, accel_msg.header.stamp, accel_msg); 
-    synched_bag.write(gyro_topic, gyro_msg.header.stamp, gyro_msg);
+    synched_bag.write(imu_topic, imu_msg.header.stamp, imu_msg); 
   }
 }
 
@@ -116,11 +116,10 @@ void synchronizeBag(const std::string& filename, ros::NodeHandle& nh)
   // Load unsynched rosbag
   rosbag::Bag unsynched_bag;
   unsynched_bag.open(filename, rosbag::bagmode::Read);
-  std::vector<std::string> topics; // create a vector of topics to iterate through
+  std::deque<std::string> topics; // create a deque of topics to iterate through
   topics.push_back(cam0_topic);
   topics.push_back(cam1_topic);
-  topics.push_back(accel_topic);
-  topics.push_back(gyro_topic);
+  topics.push_back(imu_topic);
   rosbag::View rosbagView(unsynched_bag, rosbag::TopicQuery(topics));
   cout << "Opening unsynched bag file." << endl;
 
@@ -131,17 +130,19 @@ void synchronizeBag(const std::string& filename, ros::NodeHandle& nh)
   // Set up message_filters subscribers to capture messages from the bag
   message_filters::Subscriber<sensor_msgs::Image> img0_sub(nh, cam0_topic, 10); 
   message_filters::Subscriber<sensor_msgs::Image> img1_sub(nh, cam1_topic, 10);
-  message_filters::Subscriber<geometry_msgs::Vector3Stamped> accel_sub(nh, accel_topic, 20); 
-  message_filters::Subscriber<geometry_msgs::Vector3Stamped> gyro_sub(nh, gyro_topic, 20);
+  message_filters::Subscriber<sensor_msgs::Imu> imu_sub(nh, imu_topic, 20); 
   
   // Create Approximate Time Synchronizer
-  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image, geometry_msgs::Vector3Stamped, geometry_msgs::Vector3Stamped> approxTimePolicy;
-  //ros::Duration maxInterval = ros::Duration(0.5,0);
-  //message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image>::setMaxIntervalDuration(maxInterval); // set maximum synchronization timestamp interval to (seconds, nanoseconds)
-  message_filters::Synchronizer<approxTimePolicy> sync(approxTimePolicy(100), img0_sub, img1_sub, accel_sub, gyro_sub);
-  sync.registerCallback(boost::bind(&SynchCallback, _1, _2, _3, _4));
+  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::Imu> approxTimePolicy;
+  //ros::Duration maxInterval = ros::Duration(0.5,0); // (seconds, nanoseconds)
+  //message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::Imu>::setMaxIntervalDuration(maxInterval); // set maximum synchronization timestamp difference 
+  message_filters::Synchronizer<approxTimePolicy> sync(approxTimePolicy(100), img0_sub, img1_sub, imu_sub);
+  sync.registerCallback(boost::bind(&SynchCallback, _1, _2, _3));
 
-  // Iterate through all messages on all topics in the bag and send them to the synchronizer callback
+  // Register the IMU Buffer Callback
+  imu_sub.registerCallback(IMU_Buffer_Callback);
+
+  // Iterate through all messages on all topics in the bag and send them to their callbacks
   cout << "Writing to synched bag file. This will take a few minutes..." << endl;
   BOOST_FOREACH(rosbag::MessageInstance const msg, rosbagView)
   {
@@ -157,26 +158,19 @@ void synchronizeBag(const std::string& filename, ros::NodeHandle& nh)
     {
       sensor_msgs::Image::ConstPtr img1 = msg.instantiate<sensor_msgs::Image>();
       if (img1 != NULL)
-        img1_sub.signalMessage(img1); 
+        img1_sub.signalMessage(img1); // call the SynchCallback
         j += 1;
     }
 
-    if (msg.getTopic() == accel_topic)
+    if (msg.getTopic() == imu_topic)
     {
-      geometry_msgs::Vector3Stamped::ConstPtr accel = msg.instantiate<geometry_msgs::Vector3Stamped>();
-      if (accel != NULL)
-        accel_sub.signalMessage(accel); 
+      sensor_msgs::Imu::ConstPtr imu = msg.instantiate<sensor_msgs::Imu>();
+      if (imu != NULL)
+        imu_sub.signalMessage(imu); // call the SynchCallback and IMU_Buffer_Callback
         k += 1;
     }
 
-    if (msg.getTopic() == gyro_topic)
-    {
-      geometry_msgs::Vector3Stamped::ConstPtr gyro = msg.instantiate<geometry_msgs::Vector3Stamped>();
-      if (gyro != NULL)
-        gyro_sub.signalMessage(gyro); 
-        l += 1;
-    }
-    writeToBag(synched_bag); // write to the rosbag (disk) and empty the vector as callbacks are made to save RAM space
+    writeToBag(synched_bag); // write to rosbag (disk) and empty the deques as callbacks are made to save RAM space
   }
 
   unsynched_bag.close();
@@ -193,20 +187,12 @@ int main(int argc, char** argv)
   ros::NodeHandle nh;
 
   synchronizeBag(rosbagFolderPath+"/"+unsynchedBagName, nh);
-   
-  // Register the IMU_buffer callback
-  // can you register the same callback with two subscribers?
-  // possibly just use /imu as the topic and then separate the topics further down the line when needed
-  // but the imu and acceleration might not be exactly synched themselves?
-  //accel_sub.registerCallback(IMU_BufferCallback);
-  //gyro_sub.registerCallback(IMU_BufferCallback);
 
   cout << "Total img0 callbacks = " << i << endl;
   cout << "Total img1 callbacks = " << j << endl;
-  cout << "Total accel callbacks = " << k << endl;
-  cout << "Total gyro callbacks = " << l << endl;
+  cout << "Total imu callbacks = " << k << endl;
   cout << "Total synched messages = " << synch_cnt << endl;
-  cout << "Ctrl+C to kill the node." << endl;
+  cout << "Press Ctrl+C to kill the node." << endl;
 
   ros::spin();
   return 0;
